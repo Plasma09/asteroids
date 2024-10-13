c81b841a74c33042813a7a9fa8b206c8bcbf9724
Plasma09 <malinurcameron@gmail.com>
2024-10-13 11:46:22 +0200

    3: init the game clock to time.deltatime

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
+# Import all requirements
 import pygame
 from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # Create the game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(0)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
